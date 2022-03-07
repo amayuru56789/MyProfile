23d15f2914be3ca6dde75c094bc56b9cc3472d4f
amayuru56789 <indeewara5678@gmail.com>
2022-03-07 22:27:49 +0530

    create save item function for SPA

--- myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
+++ myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
@@ -1,3 +1,19 @@
 $("#btnItem").click(function() {
-    alert("Hi");
+    saveItem();
 });
+
+function saveItem(){
+    var itemCode = $("#txtCode").val;
+    var itemName = $("#txtItemName").val;
+    var price = $("#txtPrice").val;
+    var qty = $("#txtQty").val;
+
+    var itemOb = {
+        code:itemCode,
+        name:itemName,
+        price:price,
+        qty:qty
+    }
+
+    itemDB.push(itemOb)
+}
