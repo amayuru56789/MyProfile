72768b268e00f7c6a3766eb130a65fd018cc7431
amayuru56789 <indeewara5678@gmail.com>
2022-03-07 22:48:01 +0530

    create loadAllItems function for added table row

--- myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
+++ myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
@@ -1,19 +1,28 @@
 $("#btnItem").click(function() {
     saveItem();
+    loadAllItems();
 });
 
 function saveItem(){
-    var itemCode = $("#txtCode").val;
-    var itemName = $("#txtItemName").val;
-    var price = $("#txtPrice").val;
-    var qty = $("#txtQty").val;
+    var itemCode = $("#txtCode").val();
+    var itemName = $("#txtItemName").val();
+    var price = $("#txtPrice").val();
+    var qty = $("#txtQty").val();
 
     var itemOb = {
         code:itemCode,
         name:itemName,
         price:price,
         qty:qty
-    }
+    };
 
-    itemDB.push(itemOb)
+    itemDB.push(itemOb);
+}
+
+function loadAllItems(){
+    $("#itemTable").empty();
+    for(var i=0; i<itemDB.length; i++){
+        var row = `<tr><td>${itemDB[i].code}</td><td>${itemDB[i].name}</td><td>${itemDB[i].price}</td><td>${itemDB[i].qty}</td></tr>`;
+        $("#itemTable").append(row);
+    }
 }
