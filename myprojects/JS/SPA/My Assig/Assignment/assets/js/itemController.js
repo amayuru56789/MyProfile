3edcf36328977cbd877749b95c0bac478819c23d
amayuru56789 <indeewara5678@gmail.com>
2022-03-07 23:13:05 +0530

    added event for search btn

--- myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
+++ myprojects/JS/SPA/My Assig/Assignment/assets/js/itemController.js
@@ -27,10 +27,24 @@ function loadAllItems(){
     }
 }
 
+$("#button-addon3").click(function(){
+    var searchCode = $("#txtsearchCustId").val();
+    var response = searchItem(searchCode);
+    if(response){
+        $("#txtCode").val(response.code);
+        $("#txtItemName").val(response.name);
+        $("#txtPrice").val(response.price);
+        $("#txtQty").val(response.qty);
+    }else{
+        /*clearAll();*/
+        alert("No such a Item");
+    }
+});
+
 function searchItem(code){
     for(let i=0; i<itemDB.length; i++){
         if(itemDB[i].code==code){
-            return customerDB[i];
+            return itemDB[i];
         }
     }
 }
